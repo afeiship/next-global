f9a9fc40e1ad83f614f2f0d99788443d04cf8a6a
afeiship <1290657123@qq.com>
2019-06-25 12:57:52 +0800

    add: new options

--- dist/next-global.js
+++ dist/next-global.js
@@ -8,17 +8,23 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var DEFAULT_OPTIONS = {
+    key: '$global',
+    globalKey: '__$GLOBAL__'
+  };
 
-  nx.global = function(inInitialData) {
-    nx.__$GLOBAL__ = inInitialData || {};
-    nx.defineProperty(nx, '$global', {
+  nx.global = function(inInitialData, inOptions) {
+    var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
+    var globalKey = options.globalKey;
+    nx[globalKey] = inInitialData || {};
+    nx.defineProperty(nx, options.key, {
       set: function(inValue) {
         nx.each(inValue, function(key, value) {
-          nx.set(nx.__$GLOBAL__, key, value);
+          nx.set(nx[globalKey], key, value);
         });
       },
       get: function() {
-        return nx.__$GLOBAL__;
+        return nx[globalKey];
       }
     });
   };
--- test/test.js
+++ test/test.js
@@ -1,9 +1,20 @@
 var nx = require('next-js-core2');
 require('../src/next-global');
 
-test('nx.global set/get value', function() {
-  nx.global(null);
-  nx.$global = { abc: 123 };
-  nx.$global = { 'c.d.e': 'test-str' };
-  expect(nx.$global).toEqual({ abc: 123, c: { d: { e: 'test-str' } } });
+describe('Basic test', () => {
+  test('nx.global set/get value', () => {
+    // init global:
+    nx.global(null);
+    nx.$global = { abc: 123 };
+    nx.$global = { 'c.d.e': 'test-str' };
+    expect(nx.$global).toEqual({ abc: 123, c: { d: { e: 'test-str' } } });
+  });
+
+  test('nx.global create other globalKey property', () => {
+    // init global:
+    nx.global(null, { globalKey: '__MYGOD__', key: '$god' });
+    nx.$god = { abc: 123 };
+    nx.$god = { 'c.d.e': 'test-str' };
+    expect(nx.$god).toEqual({ abc: 123, c: { d: { e: 'test-str' } } });
+  });
 });
