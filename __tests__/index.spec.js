ea819cd1840cb639e5b231cbcbd000b62e6c32e7
afeiship <1290657123@qq.com>
2023-03-01 09:16:21 +0800

    Release 1.0.1

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,9 +1,19 @@
-require('../src/index.pack');
-
-jest.setTimeout(60 * 1000);
+require('../src');
 
 describe('api.basic test', () => {
-  test('nx.boilerplatePackage', function () {
-    console.log('hello next');
+  test('setValue', function () {
+    // initial:
+    nx.global(null);
+    // set or set by path:
+    nx.$global = { abc: 123 };
+    expect(nx.get(nx.$global, 'abc')).toBe(123);
+  });
+
+  test('setByPath', function () {
+    // initial:
+    nx.global(null);
+    // set or set by path:
+    nx.$global = { 'c.d.e': 'test-str' };
+    expect(nx.get(nx.$global, 'c.d')).toEqual({ e: 'test-str' });
   });
 });
